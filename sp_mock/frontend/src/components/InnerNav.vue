e4661c28ddc34f31745eb3cfd2c5adff2e3b8161
NJavokhir <nematovjavohir58@gmail.com>
2023-09-26 19:07:18 +0500

    Moved navbar to separate component

---
+++ sp_mock/frontend/src/components/InnerNav.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import { RouterLink } from "vue-router";
+import { ref } from "vue";
+
+const poems = ref([
+  {
+    id: 1,
+    name: "Poem 1",
+    toName: "/poems/1",
+  },
+  {
+    id: 2,
+    name: "Poem 2",
+    toName: "/poems/2",
+  },
+  {
+    id: 3,
+    name: "Poem 3",
+    toName: "/poems/3",
+  },
+  {
+    id: 4,
+    name: "Poem 4",
+    toName: "/poems/4",
+  },
+  {
+    id: 5,
+    name: "Poem 5",
+    toName: "/poems/5",
+  },
+]);
+</script>
+
+<template>
+  <nav class="flex justify-center mb-16">
+    <div v-for="(i, index) in poems" :key="index">
+      <RouterLink
+        :to="i.toName"
+        class="cursor-pointer px-4 py-2 hover:bg-green-300 hover:scale-105 hover:text-white select-none"
+        >{{ i.name }}</RouterLink
+      >
+    </div>
+  </nav>
+</template>
+
+<style scoped></style>
--- sp_mock/frontend/src/views/HomeView.vue
+++ sp_mock/frontend/src/views/HomeView.vue
@@ -1,43 +1,12 @@
 <script setup>
-import { ref } from "vue";
-
-const poems = ref([
-  {
-    id: 1,
-    name: "Poem 1",
-  },
-  {
-    id: 2,
-    name: "Poem 2",
-  },
-  {
-    id: 3,
-    name: "Poem 3",
-  },
-  {
-    id: 4,
-    name: "Poem 4",
-  },
-  {
-    id: 5,
-    name: "Poem 5",
-  },
-]);
+import InnerNav from '../components/InnerNav.vue';
 </script>
 
 <template>
   <div>
-    <div class="flex justify-center mb-16">
-      <div
-        v-for="(i, index) in poems"
-        :key="i"
-        class="cursor-pointer px-4 py-2 hover:bg-green-300 hover:scale-105 hover:text-white select-none"
-      >
-        {{ i.name }}
-      </div>
-    </div>
     <div>
       <h1 class="font-extrabold text-2xl text-center mb-8">Poem title</h1>
+      <InnerNav/>
       <div>
         <p class="font-normal text-md mx-16">
           Poem body Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum

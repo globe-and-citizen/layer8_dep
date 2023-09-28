a01a93064e58e64774021d4a1360ec3c6939174d
stravid87 <ravi.student.dev@gmail.com>
2023-09-28 18:37:30 -0400

    Interceptor and Middleware now successfully transit the proxy.

--- proxy/handlers/tunnel.go
+++ proxy/handlers/tunnel.go
@@ -9,15 +9,8 @@ import (
 // Tunnel forwards the request to the service provider's backend
 func Tunnel(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("\n\n*************")
-	fmt.Println(r.Method) // >GET >POST
-	fmt.Println(r.URL)    // http://localhost:5000/api/v1 => /api/v1
-	//fmt.Println(io.ReadAll(r.Body)) // > [] <nil>
-	fmt.Println(r)
-
-	// get the service provider's backend url
-	// host := r.Header.Get("X-Forwarded-Host")
-	// scheme := r.Header.Get("X-Forwarded-Proto")
-	// url := scheme + "://" + host + r.URL.Path
+	fmt.Println(r.Method) // > GET  | > POST
+	fmt.Println(r.URL)    // (http://localhost:5000/api/v1 ) > /api/v1
 
 	backendURL := fmt.Sprintf("http://localhost:8000%s", r.URL)
 
@@ -30,20 +23,20 @@ func Tunnel(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// add headers
-	// req.Header.Add("X-Layer8-Proxy", "true")
 	for k, v := range r.Header {
 		req.Header[k] = v
 	}
 
 	// send the request
 	res, err := http.DefaultClient.Do(req)
+
 	if err != nil {
 		fmt.Println("Error sending request:", err)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	fmt.Println("Received response from 8000:", backendURL, " of code: ", res.StatusCode)
+	fmt.Println("\nReceived response from 8000:", backendURL, " of code: ", res.StatusCode)
 
 	// copy response back
 	for k, v := range res.Header {

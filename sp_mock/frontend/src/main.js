6478c7fc25c8ad7e49eb4ffa395be3d893d0d76d
NJavokhir <nematovjavohir58@gmail.com>
2023-09-29 20:58:18 +0500

    removed express-generator from backend and tailwindcss from frontend

--- sp_mock/frontend/src/main.js
+++ sp_mock/frontend/src/main.js
@@ -3,7 +3,6 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import './assets/css/tailwind.css';
 
 const app = createApp(App)
 

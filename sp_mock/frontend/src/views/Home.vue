54730b358d928b0b479d00e389d2bdca8c074f8c
Hermann EHO <hermannleboss@gmail.com>
2024-01-07 19:00:13 +0000

    feat: clean style

--- sp_mock/frontend/src/views/Home.vue
+++ sp_mock/frontend/src/views/Home.vue
@@ -11,7 +11,6 @@ let nextPoem = ref({});
 // const BackendURL = "https://container-service-3.gej3a3qi2as1a.ca-central-1.cs.amazonlightsail.com";
 const BackendURL = "http://localhost:5001";
 
-
 const userName = computed(() => {
   if (SpToken.value && SpToken.value.split(".").length > 1) {
     const payload = JSON.parse(atob(SpToken.value.split(".")[1]));
@@ -56,76 +55,47 @@ const logoutUser = () => {
 
 <template>
   <Navbar></Navbar>
-  <div id="app">
-    <div v-if="isLoggedIn" class="welcome-container">
-      <h1
-        style="color: rgb(136, 136, 136); font-weight: 600; padding-bottom: 2%"
-      >
-        Welcome {{ userName }}!
-      </h1>
-      <h3
-        style="color: rgb(136, 136, 136); font-weight: 600; padding-bottom: 2%"
-      >
-        Your MetaData:
-      </h3>
+  <div class="">
+    <div v-if="isLoggedIn" >
+      <h1>Welcome {{ userName }}!</h1>
+      <h3>Your MetaData:</h3>
       <h4
-        style="color: rgb(136, 136, 136); font-weight: 600"
         v-if="metaData.displayName"
       >
         Username: {{ metaData.displayName }}
       </h4>
       <h4
-        style="color: rgb(136, 136, 136); font-weight: 600"
         v-if="metaData.countryName"
       >
         Country: {{ metaData.countryName }}
       </h4>
       <!-- Change here -->
       <h4
-        style="color: rgb(136, 136, 136); font-weight: 600"
-        
         v-if="metaData.isEmailVerified === `true`"
       >
         Email Verified: Email is verified!
       </h4>
-      <h4 style="color: rgb(136, 136, 136); font-weight: 600" v-else>
+      <h4 v-else>
         Email Verified: Email is not verified!
       </h4>
       <br />
-      <div class="new-container">
+      <div>
         <button @click="getPoem">Get Next Poem</button>
         <button class="btn-secondary" @click="logoutUser">Logout</button>
       </div>
-      <div id="poems-container-2" style="color: black">
+      <div>
         <br />
         <div id="newPoem">
           <h3>Next Poem goes here:</h3>
           <div>Title:</div>
-          <p style="font-weight: bold">{{ nextPoem.title }}</p>
+          <p >{{ nextPoem.title }}</p>
           <div>Author:</div>
-          <p style="font-weight: bold">{{ nextPoem.author }}</p>
+          <p>{{ nextPoem.author }}</p>
           <div>Body:</div>
-          <p style="font-weight: bold">{{ nextPoem.body }}</p>
+          <p >{{ nextPoem.body }}</p>
         </div>
       </div>
     </div>
   </div>
   <div></div>
 </template>
-
-<style scoped>
-#app {
-  font-family: "Arial", sans-serif;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 80vh;
-  width: 100vh;
-  background-color: #f4f4f4;
-}
-
-.welcome-container {
-  text-align: center;
-  width: 100%;
-}
-</style>

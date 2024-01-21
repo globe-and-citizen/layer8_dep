71d8c46655f99d9d6d62c41cbef12f06822227f9
stravid87 <ravi.student.dev@gmail.com>
2024-01-21 11:06:16 -0500

    interim (main_test.go): Addition of RegisterClientHandler Test

--- server/main_test.go
+++ server/main_test.go
@@ -10,28 +10,18 @@ import (
 	"net/http/httptest"
 	"testing"
 
-	"globe-and-citizen/layer8/proxy/resource_server/controller"
+	"github.com/google/uuid"
+
+	ctl "globe-and-citizen/layer8/server/resource_server/controller"
+	"globe-and-citizen/layer8/server/resource_server/utils"
 )
 
 func TestPostgresConnection(t *testing.T) {
 	config.InitDB()
-	t.Log(*config.DB)
-}
-
-func TestIndex(t *testing.T) {
-	r, err := http.NewRequest("GET", "http://localhost:5001/", nil)
-	if err != nil {
-		t.Fatal(err)
+	if config.DB.Error != nil {
+		t.Fatalf("error connecting the db: %s", config.DB.Error.Error())
 	}
-
-	rwr := httptest.NewRecorder()
-	handler := http.HandlerFunc(Router)
-	handler.ServeHTTP(rwr, r)
-
-	status := rwr.Code
-	if status != http.StatusOK {
-		t.Errorf("Index was not available. Status code got <%v> wanted <%v>", status, http.StatusOK)
-	}
+	t.Log("Database successfully connected")
 }
 
 func TestLoginUserHandler(t *testing.T) {
@@ -41,7 +31,7 @@ func TestLoginUserHandler(t *testing.T) {
 		t.Fatal(err)
 	}
 	rwr := httptest.NewRecorder()
-	handler := http.HandlerFunc(controller.LoginPrecheckHandler)
+	handler := http.HandlerFunc(ctl.LoginPrecheckHandler)
 	handler.ServeHTTP(rwr, r)
 
 	t.Logf("%v", rwr.Body)
@@ -67,7 +57,7 @@ func TestLoginUserHandler(t *testing.T) {
 		t.Fatal(err)
 	}
 	rwr2 := httptest.NewRecorder()
-	handler2 := http.HandlerFunc(controller.LoginUserHandler)
+	handler2 := http.HandlerFunc(ctl.LoginUserHandler)
 	handler2.ServeHTTP(rwr2, r2)
 
 	status := rwr2.Code
@@ -79,27 +69,58 @@ func TestLoginUserHandler(t *testing.T) {
 	if status != http.StatusOK {
 		t.Log("Bro ur nearly there...", status)
 	}
-
 }
 
-func dep_TestDatabase(t *testing.T) {
-	r, err := http.NewRequest("POST", "http://localhost:5001/login", nil)
+func TestRegisterClientHandler(t *testing.T) {
+	// Init the DB Connection
+	config.InitDB()
+	// Create a mock request with Name & RedirectURI as the body.
+	uuid_obj := uuid.New()
+	uuid_str := uuid_obj.String()
+	jsonBodyAsString := fmt.Sprintf(`{"name":"new-client-%s","redirect_uri":"anc.com"}`, uuid_str[0:4])
+	reqBody := []byte(jsonBodyAsString)
+	reqBodyReader := bytes.NewBuffer(reqBody)
+	req, err := http.NewRequest("POST", "http://localhost:50001/api/v1/login-user", reqBodyReader)
 	if err != nil {
-		t.Fatal(err) // t.Log + T.FailNow()
+		t.Fatalf("error creating mock http request: %s", err.Error())
+	}
+
+	// Use an adapter to turn the function under test into a http.Handler
+	rwr := httptest.NewRecorder()
+	handler := http.HandlerFunc(ctl.RegisterClientHandler)
+	handler.ServeHTTP(rwr, req)
+
+	if rwr.Code != 200 {
+		t.Fatalf("req failed. rwr.Code received was: %d", rwr.Code)
+	}
+
+	rawResponse, err := io.ReadAll(rwr.Body)
+	if err != nil {
+		t.Fatalf("failed to read response body: %s", err.Error())
+	}
+
+	response := &utils.Response{}
+	if err := json.Unmarshal(rawResponse, response); err != nil {
+		t.Fatalf("unable to unmarshal rawData: %s", err.Error())
+	}
+	t.Log(response.Status)
+	t.Log(response.Error)
+	t.Log(response.Data)
+	t.Log(response.Message)
+}
+
+func TestIndex(t *testing.T) { // Router no longer exists
+	r, err := http.NewRequest("GET", "http://localhost:5001/", nil)
+	if err != nil {
+		t.Fatal(err)
 	}
 
 	rwr := httptest.NewRecorder()
-	handler := http.HandlerFunc(Router)
+	handler := http.HandlerFunc(ctl.IndexHandler)
 	handler.ServeHTTP(rwr, r)
 
-	t.Logf("%v", rwr)
+	status := rwr.Code
+	if status != http.StatusOK {
+		t.Errorf("Index was not available. Status code got <%v> wanted <%v>", status, http.StatusOK)
+	}
 }
-
-// inline struct declaration
-// pcResp := struct {
-// 	username string
-// 	salt string
-// }{
-// 	username: "value",
-// 	salt: "Shark",
-// }

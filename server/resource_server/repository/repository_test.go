610a96f36f41579741ba5db6c949f4ea3ab8442c
huzaifamk <malikhuzaifa99@gmail.com>
2024-02-12 01:24:36 +0500

    Updated unit tests of repository layer

--- server/resource_server/repository/repository_test.go
+++ server/resource_server/repository/repository_test.go
@@ -68,7 +68,7 @@ func TestRegisterClient(t *testing.T) {
 
 func TestGetClientData(t *testing.T) {
 	// Create a new mock DB and a GORM database connection
-	mockDB, mock, err := sqlmock.New()
+	mockDB, _, err := sqlmock.New()
 	if err != nil {
 		t.Fatal("Failed to create mock DB:", err)
 	}
@@ -85,19 +85,9 @@ func TestGetClientData(t *testing.T) {
 	// Define a test client name
 	testClientName := "testclient"
 
-	// Define the expected result
-	mock.ExpectQuery(`SELECT * FROM "clients" WHERE name = $1 ORDER BY "clients"."id" LIMIT 1`).
-		WithArgs(testClientName).
-		WillReturnRows(sqlmock.NewRows([]string{"id", "secret", "name", "redirect_uri"}).
-			AddRow("1", "testsecret", "testclient", "https://gcitizen.com/callback"))
-
-	// Expect a commit to be made
-	mock.ExpectCommit()
 	// Call the GetClientData function
 	repo.GetClientData(testClientName)
 
-	// Ensure all expectations were met
-	// assert.NoError(t, mock.ExpectationsWereMet())
 }
 
 func TestLoginPreCheckUser(t *testing.T) {
@@ -121,16 +111,8 @@ func TestLoginPreCheckUser(t *testing.T) {
 		Username: "test_user",
 	}
 
-	// Define the expected result
-	// mock.ExpectQuery(`SELECT * FROM "users" WHERE username = $1 ORDER BY "users"."id" LIMIT 1`).
-	// 	WithArgs(testLoginPrecheck.Username).
-	// 	WillReturnRows(sqlmock.NewRows([]string{"id", "email", "username", "first_name", "last_name", "password", "country", "display_name", "salt"}).
-	// 		AddRow())
-	// Call the LoginPreCheckUser function
 	repo.LoginPreCheckUser(testLoginPrecheck)
 
-	// Ensure all expectations were met
-	// assert.NoError(t, mock.ExpectationsWereMet())
 }
 
 func TestProfileUser(t *testing.T) {
@@ -149,16 +131,9 @@ func TestProfileUser(t *testing.T) {
 	// Create the user repository with the mock database connection
 	repo := NewRepository(db)
 
-	// Define the expected result
-	// mock.ExpectQuery(`SELECT * FROM "users" WHERE username = $1 ORDER BY "users"."id" LIMIT 1`).
-	// 	WithArgs(testProfile.Username).
-	// 	WillReturnRows(sqlmock.NewRows([]string{"id", "email", "username", "first_name", "last_name", "password", "country", "display_name", "salt"}).
-	// 		AddRow())
 	// Call the ProfileUser function
 	repo.ProfileUser(1)
 
-	// Ensure all expectations were met
-	// assert.NoError(t, mock.ExpectationsWereMet())
 }
 
 func TestVerifyEmail(t *testing.T) {
@@ -177,16 +152,9 @@ func TestVerifyEmail(t *testing.T) {
 	// Create the user repository with the mock database connection
 	repo := NewRepository(db)
 
-	// Define the expected result
-	// mock.ExpectQuery(`SELECT * FROM "users" WHERE username = $1 ORDER BY "users"."id" LIMIT 1`).
-	// 	WithArgs(testProfile.Username).
-	// 	WillReturnRows(sqlmock.NewRows([]string{"id", "email", "username", "first_name", "last_name", "password", "country", "display_name", "salt"}).
-	// 		AddRow())
 	// Call the VerifyEmail function
 	repo.VerifyEmail(1)
 
-	// Ensure all expectations were met
-	// assert.NoError(t, mock.ExpectationsWereMet())
 }
 
 func TestUpdateDisplayName(t *testing.T) {
@@ -210,14 +178,7 @@ func TestUpdateDisplayName(t *testing.T) {
 		DisplayName: "new_user",
 	}
 
-	// Define the expected result
-	// mock.ExpectQuery(`SELECT * FROM "users" WHERE username = $1 ORDER BY "users"."id" LIMIT 1`).
-	// 	WithArgs(testProfile.Username).
-	// 	WillReturnRows(sqlmock.NewRows([]string{"id", "email", "username", "first_name", "last_name", "password", "country", "display_name", "salt"}).
-	// 		AddRow())
 	// Call the UpdateDisplayName function
 	repo.UpdateDisplayName(1, testUpdateDisplayName)
 
-	// Ensure all expectations were met
-	// assert.NoError(t, mock.ExpectationsWereMet())
 }

738f91570d74abcea504928a9f6aa5608a599980
Javokhir Nematov <nematovjavohir58@gmail.com>
2024-02-21 20:11:24 +0500

    Added default styles to tailwind config

--- server/assets-v1/templates_copy/tailwind.config.js
+++ server/assets-v1/templates_copy/tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./src/**/*.{html,js}", "./public/welcome.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#ff0000',
+        secondary: '#00ff00',
+      },
+      fontFamily: {
+        sans: ['Roboto', 'Arial', 'sans-serif'],
+      },
+      spacing: {
+        '72': '18rem',
+        '84': '21rem',
+      },
+      fontSize: {
+        mammoth: '8rem'
+      }
+    },
   },
   plugins: [],
 }

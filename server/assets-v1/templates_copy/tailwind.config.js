be2a859472a8190cf4f2f4af2b8bb951d56ce4fc
Javokhir Nematov <nematovjavohir58@gmail.com>
2024-03-12 21:05:57 +0500

    Added animation, transition to portals

--- server/assets-v1/templates_copy/tailwind.config.js
+++ server/assets-v1/templates_copy/tailwind.config.js
@@ -16,6 +16,38 @@ module.exports = {
       },
       fontSize: {
         mammoth: '8rem'
+      },
+      animation: {
+        wiggle: 'wiggle 1s ease-in-out infinite',
+        animation: 'bounce 2s infinite',
+        'slideFromRight': 'slideFromRight 0.5s ease-in-out',
+        'slideFromLeft': 'slideFromLeft 0.5s ease-in-out',
+      },
+      keyframes: {
+        wiggle: {
+          "0% 100%": {
+            transform: "rotate(-1deg)",
+          },
+          "50%": { transform: "rotate(1deg)" },
+        },
+        bounce: {
+          "0%, 100%": {
+            transform: "translateY(-5%)",
+            // animation-timing-function: "cubic-bezier(0.8, 0, 1, 1)",
+          },
+          "50%": {
+            transform: "translateY(0)",
+            // animation-timing-function: "cubic-bezier(0, 0, 0.2, 1)",
+          }
+        },
+        slideFromRight: {
+          '0%': { transform: 'translateX(100%)', opacity: '0' },
+          '100%': { transform: 'translateX(0)', opacity: '1' },
+        },
+        slideFromLeft: {
+          '0%': { transform: 'translateX(-100%)', opacity: '0' },
+          '100%': { transform: 'translateX(0)', opacity: '1' },
+        },
       }
     },
   },
